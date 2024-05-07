9df403911d427cb5e0cc8e8d3ffe997b10ce7f47
HollowHole <1500175927@qq.com>
2024-05-07 14:42:00 +0800

    最后的波纹

--- Assets/Scripts/MainScene/Manager/CountryManager.cs
+++ Assets/Scripts/MainScene/Manager/CountryManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mathf
@@ -65,3 +65,4 @@ public class CountryManager : MonoBehaviour
         
     }
 }
+*/
